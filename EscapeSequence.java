94bfa967b115484031f582a0f4dcb8b1854a1f26
AMRUDESH PS <126590195+AMRUDESHPS@users.noreply.github.com>
2024-12-07 20:11:32 +0530

    Create EscapeSequence.java

---
+++ EscapeSequence.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("What do you mean by \"alga\"");
+		System.out.println("The file is located at ~\\Desktop\\Downloads");
+		System.out.println("This is a tab space \t!");
+		System.out.println("This \n is \n on \n multiple lines");
+	}
+}
