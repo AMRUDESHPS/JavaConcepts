fbc9e3ae61dd8d9d84dc26da44ac3205f634709f
AMRUDESH PS <126590195+AMRUDESHPS@users.noreply.github.com>
2024-12-08 21:38:53 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	String result = reverse("chenna line to perform reverse operation");
+	System.out.println(result);
+	
+	public static String reverse(String word) {
+		char[] individualLetters = new char[word.length()];
+		int index = 0;
+		for(int i = string.length() - 1; i >= 0; i--) {
+			individualLetters[index] = word.charAt(i);
+			index++;
+		}
+		String reversed = "";
+		for(int i = 0; i < word.length(); i++) {
+			reversed = reversed + individualLetters[i];
+		}
+		return reverse;
+	}
+
+	return 0;
+}
